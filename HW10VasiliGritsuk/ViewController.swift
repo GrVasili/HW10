6742aa2ce5b19b68af0ff958b492908addbd0c90
GrVasili <gritsuk09@gmail.com>
2021-09-12 19:31:05 +0300

    Initial Commit

---
+++ HW10VasiliGritsuk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10VasiliGritsuk
+//
+//  Created by Vasili on 12.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
